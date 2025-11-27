1e7b86a60e25c69480cdb33d4ee62305232dad5f
amalipalal <palal.asare@amalitech.com>
2025-11-27 11:09:16 +0000

    add account listing display
    - add helper for checking account display
    - add helper for savings account display

--- src/com/amalitech/bankmanagement/main/util/DisplayUtil.java
+++ src/com/amalitech/bankmanagement/main/util/DisplayUtil.java
@@ -55,6 +55,54 @@ public class DisplayUtil {
         System.out.println("Current Balance: " + displayAmount(account.getBalance()));
     }
 
+    public static void displayAccountListing(Account[] accounts) {
+        String columnFormat = "| %-15s | %-25s | %-10s | %-15s | %-10s |%n";
+
+        System.out.println("-".repeat(DISPLAY_STROKE_LENGTH));
+        System.out.printf(columnFormat, "ACC NO", "CUSTOMER NAME", "TYPE", "BALANCE", "STATUS");
+        System.out.println("-".repeat(DISPLAY_STROKE_LENGTH));
+
+        for (Account account : accounts) {
+            if (account instanceof SavingsAccount) {
+                displaySavingsAccountRow((SavingsAccount) account, columnFormat);
+            } else if (account instanceof CheckingAccount) {
+                displayCheckingAccountRow((CheckingAccount) account, columnFormat);
+            }
+
+            System.out.println("-".repeat(DISPLAY_STROKE_LENGTH));
+        }
+    }
+
+    private static void displayCheckingAccountRow(CheckingAccount account, String columnFormat) {
+        String accountNumber = account.getAccountNumber();
+        String overDraftLimit = displayDecimal(account.getOverDraftLimit());
+        String customerName = account.getCustomer().getName();
+        String accountType = account.getAccountType();
+        String monthlyFee = displayAmount(account.getMonthlyFee());
+        String balance = displayAmount(account.getBalance());
+        String status = account.getStatus();
+
+        String customerDetails = customerName + "\n" + overDraftLimit;
+        String typeDetails = accountType + "\n" + monthlyFee;
+
+        System.out.printf(columnFormat, accountNumber, customerDetails, typeDetails, balance, status);
+    }
+
+    private static void displaySavingsAccountRow(SavingsAccount account, String columnFormat) {
+        String accountNumber = account.getAccountNumber();
+        String interestRate = displayDecimal(account.getInterestRate());
+        String customerName = account.getCustomer().getName();
+        String accountType = account.getAccountType();
+        String minimumBalance = displayAmount(account.getMinimumBalance());
+        String balance = displayAmount(account.getBalance());
+        String status = account.getStatus();
+
+        String customerDetails = customerName + "\n" + "Interest Rate" + interestRate;
+        String typeDetails = accountType + "\n" + "Min Balance: " + minimumBalance;
+
+        System.out.printf(columnFormat, accountNumber, customerDetails, typeDetails, balance, status);
+    }
+
     public static void displayTransaction(Transaction transaction) {
         double previousAccountBalance = transaction.getBalanceAfter() - transaction.getAmount();
 
